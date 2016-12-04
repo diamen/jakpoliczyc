6aa3227545c8a97bbb52a484662391bc77872049
diamen <mstopa23@gmail.com>
2016-12-04 21:55:35 +0100

    Poprawnie działająca dyrektywa rozwijającego menu

--- web/scripts/directives/jpmenu.js
+++ web/scripts/directives/jpmenu.js
@@ -1,20 +1,20 @@
 angular.module('jakPoliczycDirectives')
-    .directive('jpmenu', ['$compile', function ($compile) {
+    .directive('jpmenu', function () {
         return {
             restrict: 'E',
             scope: {
                 items: '@'
             },
-            link: function(scope, element) {
+            link: function(scope) {
                 scope.nitems = angular.fromJson(scope.items);
             },
             template:
             "<div id='tree-root' class='jpmenu' ui-tree data-drag-enabled='false'>" +
                 "<ol ui-tree-nodes='' ng-model='nitems' ng-class='{hidden: collapsed}'>" +
-                    "<li ng-repeat='item in nitems track by $index' ui-tree-node jpnode item='{{item}}'>" +
+                    "<li ng-repeat='item in nitems track by $index' ui-tree-node ng-include=\"'/views/templates/jpnode.html'\">" +
                     "</li>" +
                 "</ol>" +
             "</div>"
         };
 
-    }]);
+    });
