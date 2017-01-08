7d79bb303689b3eae5013996abb0bf22cb5e9948
diamen <mstopa23@gmail.com>
2017-01-08 18:27:10 +0100

    Dopracowanie dyrektywy jparticle. Dodanie mozliwości edytowania artykułów + usuwania komentarzy.

--- web/scripts/directives/jparticle.js
+++ web/scripts/directives/jparticle.js
@@ -1,5 +1,5 @@
 angular.module('jakPoliczycDirectives')
-    .directive('jparticle', ['$sanitize', 'jpparser', 'jpvalidator', function ($sanitize, jpparser, jpvalidator) {
+    .directive('jparticle', ['$sanitize', '$sce', '$window', 'jpparser', 'jpvalidator', function ($sanitize, $sce, $window, jpparser, jpvalidator) {
         return {
             restrict: 'E',
             scope: {
@@ -7,22 +7,55 @@ angular.module('jakPoliczycDirectives')
                 add: '&'
             },
             templateUrl: '/views/templates/jparticle.html',
-            link: function($scope) {
+            link: function($scope, $element) {
+                var editables = [];
+                $scope.isEditable = false;
+
                 $scope.article = angular.fromJson($scope.article);
                 $scope.nauthor = "Małgorzata Kotarska";
                 $scope.ndate = $scope.article.date;
+                $scope.nintro = $scope.article.intro;
                 $scope.ntitle = $scope.article.title;
                 $scope.pattern = {};
                 $scope.pattern.user = "[a-zA-Z0-9]+";
                 $scope.pattern.content = new RegExp('^((?!http:\/\/)(?!https:\/\/)(?!www\.).)*$', "im");
                 $scope.ncomments = angular.fromJson($scope.article.comments);
                 $scope.ntags = angular.fromJson($scope.article.tags);
-                $scope.ncontents = jpparser.parseTextBlock($scope.article.content);
+                $scope.ncontent = $sce.trustAsHtml($scope.article.content);
                 $scope.noturls = jpvalidator.noturls;
 
                 $scope.nadd = function (author, content) {
                   $scope.add({author: author, content: $sanitize(content)});
                 };
+
+                $scope.submit = function () {
+                  // TODO
+                };
+
+                $scope.edit = function () {
+                    $scope.isEditable = true;
+
+                    if (editables.length === 0) {
+                        angular.forEach($element[0].querySelectorAll('[contenteditable="false"'), function (raw) {
+                            editables.push(angular.element(raw));
+                        });
+                    }
+                    toggleEditable(true);
+                };
+
+                $scope.cancel = function () {
+                  $window.location.reload();
+                };
+
+                $scope.remove = function () {
+                    // TODO
+                };
+
+                function toggleEditable(state) {
+                    angular.forEach(editables, function (editable) {
+                        editable.attr('contenteditable', state);
+                    });
+                }
             }
         };
 
