0aee9129357bd22afa8fa6828ac576df3da124f8
diamen <mstopa23@gmail.com>
2017-01-19 20:28:47 +0100

    Grunt - rozdzielenie builda na DEV i DIST

--- web/scripts/glue.js
+++ web/scripts/glue.js
@@ -1,74 +1,20 @@
-head.load(
+function loadScript(url, callback)
+{
+    var h = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
 
-    /* Libraries */
-    { file: '/libraries/angular/angular.min.js' },
-    { file: '/libraries/mspace/mspace.js' },
-    { file: '/libraries/ui-bootstrap/ui-bootstrap-tpls-2.0.0.min.js' },
-    { file: '/libraries/ui-router/angular-ui-router.min.js' },
-    { file: '/libraries/angular-mocks/angular-mocks.js' },
-    { file: '/libraries/angular-sanitize/angular-sanitize.js' },
-    { file: '/libraries/angular-ui-tree/dist/angular-ui-tree.js' },
-    { file: '/libraries/modernizr/modernizr.js' },
-    { file: '/libraries/underscore/underscore.js' },
+    script.onreadystatechange = callback;
+    script.onload = callback;
 
-    /* Browser support */
-    { file: '/scripts/browsersupport.js' },
+    h.appendChild(script);
+}
 
-    /* Common */
-    { file: '/scripts/languages/PL.js' },
-
-    /* Main */
-    { file: '/scripts/app.js' },
-
-    /* Config */
-    { file: '/scripts/config/bootstrap.js' },
-    { file: '/scripts/config/routersConfig.js' },
-    { file: '/scripts/config/treeConfig.js' },
-
-    /* Dane testowe */
-    { file: '/scripts/mockdata.js' },
-
-    /* Controllers */
-    { file: '/scripts/controllers/controller.js' },
-    { file: '/scripts/controllers/parentCtrl.js' },
-    { file: '/scripts/controllers/addCtrl.js' },
-    { file: '/scripts/controllers/articleCtrl.js' },
-    { file: '/scripts/controllers/articlesCtrl.js' },
-    { file: '/scripts/controllers/modalCtrl.js' },
-    { file: '/scripts/controllers/singleStorageCtrl.js' },
-    { file: '/scripts/controllers/storageCtrl.js' },
-    { file: '/scripts/controllers/subscribeCtrl.js' },
-    { file: '/scripts/controllers/tagCtrl.js' },
-
-    /* Directives */
-    { file: '/scripts/directives/directive.js' },
-    { file: '/scripts/directives/jpadd.js' },
-    { file: '/scripts/directives/jparticle.js' },
-    { file: '/scripts/directives/jpcategory.js' },
-    { file: '/scripts/directives/jpcategoryInput.js' },
-    { file: '/scripts/directives/jpcontact.js' },
-    { file: '/scripts/directives/jplogin.js' },
-    { file: '/scripts/directives/jpmenu.js' },
-    { file: '/scripts/directives/jpnode.js' },
-    { file: '/scripts/directives/jppostarea.js' },
-    { file: '/scripts/directives/mathjaxBind.js' },
-    { file: '/scripts/directives/slideable.js' },
-    { file: '/scripts/directives/slideToggle.js' },
-
-    /* Directives
-       -----Validators*/
-    { file: '/scripts/directives/validators/jpalphanumeric.js' },
-    { file: '/scripts/directives/validators/jpcharacters.js' },
-
-    /* Factories */
-    { file: '/scripts/factories/factory.js' },
-    { file: '/scripts/factories/jpcommon.js' },
-    { file: '/scripts/factories/jpgenerator.js' },
-
-    /* Services */
-    { file: '/scripts/services/service.js' },
-    { file: '/scripts/services/jpartfilter.js' },
-    { file: '/scripts/services/jpauth.js' },
-    { file: '/scripts/services/jpvalidator.js' },
-    { file: '/scripts/services/modalService.js' }
-);
+loadScript('../libraries/head/head.load.min.js', function () {
+    loadScript('./scripts/libraries.js', function () {
+        head.load(
+            getFiles()
+        )
+    });
+});
